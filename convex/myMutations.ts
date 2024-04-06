85bf599e0f1ca5a6defdcd08117dd320dd46d526
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2024-04-06 15:59:51 -0500

    working on fixing bugs

--- convex/myMutations.ts
+++ convex/myMutations.ts
@@ -1,4 +1,4 @@
-import { mutation } from "./_generated/server";
+import { internalMutation, internalQuery, mutation , } from "./_generated/server";
 import { ConvexError, v } from "convex/values";
 
 export const createNewConversation = mutation({
@@ -18,7 +18,6 @@ export const createNewConversation = mutation({
       userId: subject,
       title: message,
     });
-    console.log(newConversation)
     return newConversation;
   },
 });
@@ -30,13 +29,27 @@ export const sendMessage = mutation({
     const user = await ctx.auth.getUserIdentity();
     if(!user) throw new ConvexError("You are not authorized");
 
+    const conversations = 
+
+
     await ctx.db.insert("messages", {
       message,
       conversationId,
       type: "user",
       userId: user.subject
     });
+
+    
     return true;
     //  after a new messsage has been created then we wanna save the message in the db and now we can do anuthing we want with openai
   },
 });
+
+
+export const somemutation = internalMutation({
+  args:{},
+  async handler(ctx, args) {
+
+    return;
+  }
+})
