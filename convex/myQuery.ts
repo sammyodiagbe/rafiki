991bd5f073cb9f08ee44c3c6ef389526a89c6c4d
Samson Odiagbe <sam.odiagbe.dev@gmail.com>
2024-04-24 20:26:53 -0400

    fixing authentication bugs

--- convex/myQuery.ts
+++ convex/myQuery.ts
@@ -4,15 +4,19 @@ import { ConvexError, v } from "convex/values";
 export const getConversations = query({
   args: {},
   async handler(ctx) {
-    const user = await ctx.auth.getUserIdentity();
+    try {
+      const user = await ctx.auth.getUserIdentity();
 
-    if (!user) throw new ConvexError("You are not authorized");
+      if (!user) throw new ConvexError("You are not authorized");
 
-    const conversations = await ctx.db
-      .query("conversations")
-      .filter((q) => q.eq(q.field("userId"), user.subject))
-      .collect();
-    return conversations;
+      const conversations = await ctx.db
+        .query("conversations")
+        .filter((q) => q.eq(q.field("userId"), user.subject))
+        .collect();
+      return conversations;
+    } catch (error) {
+      console.log(error);
+    }
   },
 });
 
